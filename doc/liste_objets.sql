528d1fcc371993396ae2cd6809bacfb02856ca56
cedricpc <cedricpc@cedricpc.net>
2012-10-13 01:54:15 +0200

    * Modification légère des notices légales dans les en-têtes.

--- doc/liste_objets.sql
+++ doc/liste_objets.sql
@@ -1,3 +1,20 @@
+-- ----------------------------------------------------------------------------------------
+-- EpiKnet Idle RPG (EIRPG)
+-- Copyright (C) 2005-2012 Francis D (Homer) & EpiKnet
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License version 3 as
+-- published by the Free Software Foundation.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program. if not, see <http://www.gnu.org/licenses/>.
+-- ----------------------------------------------------------------------------------------
+
 --
 -- Contenu de la table `ListeObjets`
 --
--- lib/clsDB.php
+++ lib/clsDB.php
@@ -1,22 +1,21 @@
 <?php
 
 /*
-EpiKnet Idle RPG (EIRPG)
-Copyright (C) 2005-2012 Francis D (Homer) & EpiKnet
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Affero General Public License,
-version 3 as published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * EpiKnet Idle RPG (EIRPG)
+ * Copyright (C) 2005-2012 Francis D (Homer) & EpiKnet
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. if not, see <http://www.gnu.org/licenses/>.
+ */
 
 class DB
 /**
