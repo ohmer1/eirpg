5efabf46dce9663fc1f07529e1b1dd4c485c2c70
Homer <homer@epiknet.org>
2012-11-10 22:30:56 -0500

    Les obets de base doivent être insérés en premier lors de l'insertion des éléments dans la base de données.

--- doc/liste_objets.sql
+++ doc/liste_objets.sql
@@ -28,6 +28,7 @@ INSERT INTO `ListeObjets` VALUES (6, 'gants', 0, 'N', 0, NULL, NULL);
 INSERT INTO `ListeObjets` VALUES (7, 'cuissardes', 0, 'N', 0, NULL, NULL);
 INSERT INTO `ListeObjets` VALUES (8, 'bouclier', 0, 'N', 0, NULL, NULL);
 INSERT INTO `ListeObjets` VALUES (9, 'bottes', 0, 'N', 0, NULL, NULL);
+INSERT INTO `ListeObjets` VALUES (20, 'charme', 0, 'N', 0, NULL, NULL);
 INSERT INTO `ListeObjets` VALUES (10, 'objet unique sans nom #1', 20, 'O', 30, 1, 55);
 INSERT INTO `ListeObjets` VALUES (11, 'objet unique sans nom #2', 23, 'O', 30, 2, 70);
 INSERT INTO `ListeObjets` VALUES (12, 'objet unique sans nom #3', 26, 'O', 30, 3, 85);
@@ -38,4 +39,3 @@ INSERT INTO `ListeObjets` VALUES (16, 'objet unique sans nom #7', 38, 'O', 40, 7
 INSERT INTO `ListeObjets` VALUES (17, 'objet unique sans nom #8', 41, 'O', 40, 8, 160);
 INSERT INTO `ListeObjets` VALUES (18, 'objet unique sans nom #9', 44, 'O', 40, 9, 175);
 INSERT INTO `ListeObjets` VALUES (19, 'objet unique sans nom #10', 48, 'O', 45, 20, 200);
-INSERT INTO `ListeObjets` VALUES (20, 'charme', 0, 'N', 0, NULL, NULL);
