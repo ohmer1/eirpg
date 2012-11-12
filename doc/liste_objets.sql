05e492c0d86d472e40bec6445d880778eaa0c25f
cedricpc <cedricpc@cedricpc.net>
2012-11-12 19:20:23 +0100

    * Modification des identifiants des objets pour correspondre à l'ordre d'insertion. [doc/liste_objets.sql] * Modification du fichier SQL pour insérer tous les objets en une requête. [doc/liste_objets.sql]

--- doc/liste_objets.sql
+++ doc/liste_objets.sql
@@ -19,23 +19,24 @@
 -- Contenu de la table `ListeObjets`
 --
 
-INSERT INTO `ListeObjets` VALUES (1, 'anneau', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (2, 'amulette', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (3, 'arme', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (4, 'bâton', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (5, 'tunique', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (6, 'gants', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (7, 'cuissardes', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (8, 'bouclier', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (9, 'bottes', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (20, 'charme', 0, 'N', 0, NULL, NULL);
-INSERT INTO `ListeObjets` VALUES (10, 'objet unique sans nom #1', 20, 'O', 30, 1, 55);
-INSERT INTO `ListeObjets` VALUES (11, 'objet unique sans nom #2', 23, 'O', 30, 2, 70);
-INSERT INTO `ListeObjets` VALUES (12, 'objet unique sans nom #3', 26, 'O', 30, 3, 85);
-INSERT INTO `ListeObjets` VALUES (13, 'objet unique sans nom #4', 29, 'O', 35, 4, 100);
-INSERT INTO `ListeObjets` VALUES (14, 'objet unique sans nom #5', 32, 'O', 35, 5, 115);
-INSERT INTO `ListeObjets` VALUES (15, 'objet unique sans nom #6', 35, 'O', 35, 6, 130);
-INSERT INTO `ListeObjets` VALUES (16, 'objet unique sans nom #7', 38, 'O', 40, 7, 145);
-INSERT INTO `ListeObjets` VALUES (17, 'objet unique sans nom #8', 41, 'O', 40, 8, 160);
-INSERT INTO `ListeObjets` VALUES (18, 'objet unique sans nom #9', 44, 'O', 40, 9, 175);
-INSERT INTO `ListeObjets` VALUES (19, 'objet unique sans nom #10', 48, 'O', 45, 20, 200);
+INSERT INTO `ListeObjets` (`Id_ListeObjets`, `Name`, `Minimum`, `EstUnique`, `Probabilite`, `Type`, `Niveau`) VALUES
+(1, 'anneau', 0, 'N', 0, NULL, NULL),
+(2, 'amulette', 0, 'N', 0, NULL, NULL),
+(3, 'arme', 0, 'N', 0, NULL, NULL),
+(4, 'bâton', 0, 'N', 0, NULL, NULL),
+(5, 'tunique', 0, 'N', 0, NULL, NULL),
+(6, 'gants', 0, 'N', 0, NULL, NULL),
+(7, 'cuissardes', 0, 'N', 0, NULL, NULL),
+(8, 'bouclier', 0, 'N', 0, NULL, NULL),
+(9, 'bottes', 0, 'N', 0, NULL, NULL),
+(10, 'charme', 0, 'N', 0, NULL, NULL),
+(11, 'objet unique sans nom #1', 20, 'O', 30, 1, 55),
+(12, 'objet unique sans nom #2', 23, 'O', 30, 2, 70),
+(13, 'objet unique sans nom #3', 26, 'O', 30, 3, 85),
+(14, 'objet unique sans nom #4', 29, 'O', 35, 4, 100),
+(15, 'objet unique sans nom #5', 32, 'O', 35, 5, 115),
+(16, 'objet unique sans nom #6', 35, 'O', 35, 6, 130),
+(17, 'objet unique sans nom #7', 38, 'O', 40, 7, 145),
+(18, 'objet unique sans nom #8', 41, 'O', 40, 8, 160),
+(19, 'objet unique sans nom #9', 44, 'O', 40, 9, 175),
+(20, 'objet unique sans nom #10', 48, 'O', 45, 10, 200);
